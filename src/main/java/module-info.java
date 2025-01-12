677b2d992d999728e446ffd210d6a070abded868
Cibotarescu Igor <igor.cibotarescu@pss.ro>
2025-01-12 21:24:27 +0200

    add artifact

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.ici.messengercloneclient {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
 
     opens org.ici.messengercloneclient to javafx.fxml;
